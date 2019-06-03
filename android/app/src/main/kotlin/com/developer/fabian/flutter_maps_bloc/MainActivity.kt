f87b730d00c818b27315f0924993e364142b5b25
Fabian Varela Bonett <jfabian.303@gmail.com>
2019-06-02 21:46:18 -0500

    Create a new repo with existing files

---
+++ android/app/src/main/kotlin/com/developer/fabian/flutter_maps_bloc/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.developer.fabian.flutter_maps_bloc
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Container(),
+    );
+  }
+}
