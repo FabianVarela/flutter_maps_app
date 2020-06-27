c6e85c6054e1e1b11b5da433ce2decfc00b24767
fabianvarela <jfabia.303@gmail.com>
2020-06-26 23:28:53 -0500

    Set decode polyline in utils and set env in google api key

--- lib/common/google_api_key.dart
+++ lib/common/google_api_key.dart
@@ -1,9 +1,13 @@
 import 'dart:io';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 mixin GoogleApiKey {
   String getApiKey() {
+    final DotEnv dotEnv = DotEnv();
+
     return Platform.isAndroid
-        ? ""
-        : "";
+        ? dotEnv.env['GOOGLE_MAP_API_KEY_ANDROID']
+        : dotEnv.env['GOOGLE_MAP_API_KEY_IOS'];
   }
 }
--- lib/common/utils.dart
+++ lib/common/utils.dart
@@ -1,6 +1,47 @@
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Utils {
   static  Future<String> getFileData(String path) async =>
       await rootBundle.loadString(path);
+
+  static List<LatLng> decodePolyLine(String encoded) {
+    final List<LatLng> poly = List<LatLng>();
+    final int len = encoded.length;
+
+    int index = 0;
+    int lat = 0;
+    int lng = 0;
+
+    while (index < len) {
+      int b;
+      int shift = 0;
+      int result = 0;
+
+      do {
+        b = encoded.codeUnitAt(index++) - 63;
+        result |= (b & 0x1f) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+
+      final int dLat = (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
+      lat += dLat;
+      shift = 0;
+      result = 0;
+
+      do {
+        b = encoded.codeUnitAt(index++) - 63;
+        result |= (b & 0x1f) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+
+      final int dLng = (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
+      lng += dLng;
+
+      final LatLng p = LatLng(lat.toDouble() / 1E5, lng.toDouble() / 1E5);
+      poly.add(p);
+    }
+
+    return poly;
+  }
 }
