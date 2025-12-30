1482ffe32acbe2556de96962309f0c86605f30e9
FabianVarela <jfabian.303@gmail.com>
2025-12-30 18:33:12 -0500

    Create assets generated file

---
+++ lib/core/gen/assets.gen.dart
@@ -0,0 +1,64 @@
+// dart format width=80
+
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/default_mode.png
+  String get defaultMode => 'assets/images/default_mode.png';
+
+  /// File path: assets/images/night_blue_mode.png
+  String get nightBlueMode => 'assets/images/night_blue_mode.png';
+
+  /// File path: assets/images/night_mode.png
+  String get nightMode => 'assets/images/night_mode.png';
+
+  /// File path: assets/images/personal_mode.png
+  String get personalMode => 'assets/images/personal_mode.png';
+
+  /// File path: assets/images/uber_mode.png
+  String get uberMode => 'assets/images/uber_mode.png';
+
+  /// List of all assets
+  List<String> get values => [
+    defaultMode,
+    nightBlueMode,
+    nightMode,
+    personalMode,
+    uberMode,
+  ];
+}
+
+class $AssetsMapStylesGen {
+  const $AssetsMapStylesGen();
+
+  /// File path: assets/map_styles/night_blue_mode.json
+  String get nightBlueMode => 'assets/map_styles/night_blue_mode.json';
+
+  /// File path: assets/map_styles/night_mode.json
+  String get nightMode => 'assets/map_styles/night_mode.json';
+
+  /// File path: assets/map_styles/personal_mode.json
+  String get personalMode => 'assets/map_styles/personal_mode.json';
+
+  /// File path: assets/map_styles/uber_mode.json
+  String get uberMode => 'assets/map_styles/uber_mode.json';
+
+  /// List of all assets
+  List<String> get values => [nightBlueMode, nightMode, personalMode, uberMode];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+  static const $AssetsMapStylesGen mapStyles = $AssetsMapStylesGen();
+}
