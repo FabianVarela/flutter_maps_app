cb278cac8dc51a5534f1d37a1291eff1f511eb29
FabianVarela <jfabian.303@gmail.com>
2026-04-05 00:59:53 -0500

    Update iOS files migrating ui scene

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,25 +3,28 @@ import Flutter
 import GoogleMaps
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   override func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     let dartDefinesString = Bundle.main.infoDictionary!["DART_DEFINES"] as! String
     var dartDefinesDictionary = [String:String]()
-    
+
     for definedValue in dartDefinesString.components(separatedBy: ",") {
         let decoded = String(data: Data(base64Encoded: definedValue)!, encoding: .utf8)!
         let values = decoded.components(separatedBy: "=")
         dartDefinesDictionary[values[0]] = values[1]
     }
-    
+
     let dartDefineMapKey = dartDefinesDictionary["GOOGLE_MAPS_API_KEY"] ?? ""
     let envMapKey = Bundle.main.object(forInfoDictionaryKey: "GOOGLE_MAPS_API_KEY") as? String ?? ""
-      
+
     GMSServices.provideAPIKey(dartDefineMapKey ?? envMapKey)
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
